b03dd1b3ac9c02648f0b5b90c3f1be9709510950
zhenren <wangweizhen@bilibili.com>
2018-02-19 22:19:14 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
